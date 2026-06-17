bfbe5a5eb41cc05340a1eaabdd5d1c78d44c46b4
Owemyr <47249045+Owemyr@users.noreply.github.com>
2026-06-17 13:14:11 +0200

    Added some custom styles

--- src/style/stylefunctions.js
+++ src/style/stylefunctions.js
@@ -1,9 +1,17 @@
 import defaultStyle from './stylefunctions/default';
 import ritlagerStyle from './stylefunctions/ritlager';
+import hatchYellowStyle from './stylefunctions/hatchyellow';
+import hatchPinkStyle from './stylefunctions/hatchpink';
+import hatchGreyStyle from './stylefunctions/hatchgrey';
+import hatchDotBlueStyle from './stylefunctions/hatch_dot_blue';
 
 const customStyles = {
   default: defaultStyle,
-  ritlager: ritlagerStyle
+  ritlager: ritlagerStyle,
+  hatchyellow: hatchYellowStyle,
+  hatchpink: hatchPinkStyle,
+  hatchgrey: hatchGreyStyle,
+  hatchdotblue: hatchDotBlueStyle
 };
 
 export default function styleFunctions(customStyle, params) {
---
+++ src/style/stylefunctions/hatchgrey.js
@@ -0,0 +1,45 @@
+import Style from 'ol/style/Style';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+
+const HATCH_COLOR = '#b4b0aa';
+const STROKE_WIDTH = 3;
+
+function getHatchPattern() {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  const pixelRatio = window.devicePixelRatio || 1;
+  const size = 16 * pixelRatio;
+  canvas.width = size;
+  canvas.height = size;
+  context.strokeStyle = HATCH_COLOR;
+  context.lineWidth = STROKE_WIDTH * pixelRatio;
+  context.lineCap = 'butt';
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(size, size);
+  context.moveTo(-size, 0);
+  context.lineTo(0, size);
+  context.moveTo(size, 0);
+  context.lineTo(2 * size, size);
+  context.stroke();
+  return context.createPattern(canvas, 'repeat');
+}
+
+const hatchFill = new Fill({ color: getHatchPattern() });
+const polygonStroke = new Stroke({ color: HATCH_COLOR, width: STROKE_WIDTH });
+
+export default function ritlagerStyle() {
+  return function styles(feature) {
+    const geometryType = feature.getGeometry().getType();
+
+    if (geometryType === 'Polygon' || geometryType === 'MultiPolygon') {
+      return new Style({
+        fill: hatchFill,
+        stroke: polygonStroke,
+        zIndex: 30
+      });
+    }
+    return null;
+  };
+}
